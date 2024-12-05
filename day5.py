47e3720534b68e2156bdb607b6cd302e712897a6
manwithfeathers <a.d.parkinson@gmail.com>
2024-12-05 21:52:01 +0000

    day5 part 2 completed

--- day5.py
+++ day5.py
@@ -4,10 +4,15 @@ with open("data.txt") as f:
   rules = data[:splindex]
   books = data[splindex+1:]
   books = [book.split(",") for book in books]
-new_rules =[]
+
+"""for part 2"""
+flat_rules =[]
 
 for i in range(len(rules)):
   rules[i] = rules[i].split("|")
+  """this is for part 2"""
+  for j in rules[i]:
+      flat_rules += [j]
 
 def page_checker(lst):
     patterns = []
@@ -22,14 +27,31 @@ def page_checker(lst):
             return False
     return True
 
+#part 1
+broken_pages = []
 page_list = []
 for book in books:
     if page_checker(book):
         page_list.append(int(book[len(book)//2]))
-
+    else:
+        broken_pages.append(book)
+#part 1
 print(sum(page_list))
 
+#part2
+fixed_updates = []
+for bp in broken_pages:
+    """create a dummy page we can fill"""
+    fixed_page = [1 for i in range(len(bp))]
+    for i in range(len(bp)):
+        """find index of each item in the flat rules. the length of this list can give us the correct index of the item """
+        indexes = [index for index, value in enumerate(flat_rules) if value == bp[i] and not index % 2 and flat_rules[index +1] in bp]
+        fixed_index = len(indexes)
+        fixed_page[fixed_index] = bp[i]
+    fixed_page = fixed_page[::-1]
+    fixed_updates.append(int(fixed_page[len(fixed_page)//2]))
 
+print(sum(fixed_updates))
 
 
 
